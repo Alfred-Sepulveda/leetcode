7349e5644159b3bebc553ad66a0d4d931402d8a5
Alfred-Sepulveda <006372934@coyote.csusb.edu>
2022-09-07 14:05:41 -0700

    first leetcode uploaded

---
+++ Python/leetcode1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sums = nums[i] + nums[j]
+                if sums == target:
+                    return [i,j]
+
+                    

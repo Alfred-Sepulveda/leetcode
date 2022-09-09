4f542dbc22fdaa96c74b14ec31d4686ec1117e30
Alfred-Sepulveda <006372934@coyote.csusb.edu>
2022-09-08 18:53:21 -0700

    added leetcode 20 easy

---
+++ Python/leetcode20E.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s:str) -> bool:
+        closedToOpen = {")":"(","]":"[","}":"{"}
+        stack = []
+        for i in s:
+            if i in closedToOpen:
+                if stack and stack[-1] == closedToOpen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
+
+#mistakes I made were if i in closedToOpen loop. it has to be i not s. Also stack and 
+#stack[-1] == closedToOpen[i] in order to stack.pop() or remove the bracket from the
+#stack

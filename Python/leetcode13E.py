5a6c2f123b107017d6a1a04d1d55886ee0c91fc0
Alfred-Sepulveda <006372934@coyote.csusb.edu>
2022-09-08 18:19:23 -0700

    added leetcode 13 easy

---
+++ Python/leetcode13E.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s:str) -> int:
+        roman = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        result = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                result -= roman[s[i]]
+            else:
+                result += roman[s[i]]
+        return result
+
+#for this code range(len(s)) has to be greater than i+1 or i+1 < len(s)
+#and roman[s[i]] < roman[s[i+1]] 
+#this is the only struggle I am having.

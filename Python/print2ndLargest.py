e97732e0022f69cff33407443a1afd59e5aa5b4b
Alfred-Sepulveda <006372934@coyote.csusb.edu>
2022-09-29 14:02:08 -0700

    Im just a Bot doing its Thang

--- Python/print2ndLargest.py
+++ Python/print2ndLargest.py
@@ -1,14 +1,17 @@
-def print2ndLargest(arr, arr_size):
-    if (arr_size < 2):
-        print("Invalid Input ")
-        return
-    arr.sort
+# def print2ndLargest(arr, arr_size):
+#     if (arr_size < 2):
+#         print("Invalid Input ")
+#         return
+#     arr.sort
 
-    for i in range(arr_size-2,-1,-1):
-        if (arr[i] != arr[arr_size -1]):
-            print("The second Largest element is", arr[i])
-            return
-    print("there is no second largest element")
+#     for i in range(arr_size-2,-1,-1):
+#         if (arr[i] != arr[arr_size -1]):
+#             print("The second Largest element is", arr[i])
+#             return
+#     print("there is no second largest element")
+
+
+
 
 #Driver Code
 arr = [25,-2,0,7,25,24,20]
